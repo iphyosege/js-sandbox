26d3ea562a0acfbecf696fb3d4bda3dafd9f9332
iphyosege <101004580+iphyosege@users.noreply.github.com>
2022-04-08 19:57:44 +0100

    Create object.js

---
+++ object.js
@@ -0,0 +1,29 @@
+//an object with username and password
+var user = {
+  username: "tokunbo",
+  password: "cherry"
+};
+
+//array that contains the above object(user)
+var database = [
+  {
+    username: "tokunbo",
+    password: "cherry"
+  }
+];
+
+//array which conatains three objects
+var newsfeed = [
+  {
+    username: "tokunbo",
+    timeline: "trial"
+  },
+  {
+    username: "tokunbo",
+    timeline: "trial"
+  },
+  {
+    username: "tokunbo",
+    timeline: "trial"
+  }
+];
